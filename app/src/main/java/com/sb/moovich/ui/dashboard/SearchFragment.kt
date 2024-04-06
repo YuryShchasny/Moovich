2a98e587d9fd1f87f8e699b369f3f6714cfd1658
YuryShchasny <yuryshchasnydev@gmail.com>
2024-04-06 11:34:15 +0300

    bottom_nav_menu init

--- app/src/main/java/com/sb/moovich/ui/dashboard/SearchFragment.kt
+++ app/src/main/java/com/sb/moovich/ui/dashboard/SearchFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.sb.moovich.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class SearchFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
 
--- app/src/main/java/com/sb/moovich/ui/notifications/WatchListFragment.kt
+++ app/src/main/java/com/sb/moovich/ui/notifications/WatchListFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.sb.moovich.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class WatchListFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
